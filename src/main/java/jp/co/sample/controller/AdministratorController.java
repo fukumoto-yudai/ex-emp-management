5076f3c7d71fb7f3b7f67d5fb977d863a0fbf4d0
fukumoto <yudai.fukumoto@rakus-partners.co.jp>
2022-05-13 16:14:14 +0900

    exam4-3

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -1,9 +1,12 @@
 package jp.co.sample.controller;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import jp.co.sample.domain.Administrator;
 import jp.co.sample.form.InsertAdministratorForm;
 
 @Controller
@@ -15,9 +18,14 @@ public class AdministratorController {
 		return new InsertAdministratorForm();
 	}
 	@RequestMapping("/toInsert")
-	public String toInsert() {
-		return "administrator/insert";
+	public String create(InsertAdministratorForm form
+			,RedirectAttributes redirectAttributes) {
+		Administrator administrator=new Administrator();
+		BeanUtils.copyProperties(form, administrator);
+		
+		return "redirect:/";
 	}
+	
 	
 
 }
