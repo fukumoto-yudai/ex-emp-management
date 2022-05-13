e7101cacfc491a7f8f462021ab2d5ff2f6da1e71
fukumoto <yudai.fukumoto@rakus-partners.co.jp>
2022-05-13 17:41:21 +0900

    exam6-3

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -1,7 +1,11 @@
 package jp.co.sample.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -9,10 +13,14 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import jp.co.sample.domain.Administrator;
 import jp.co.sample.form.InsertAdministratorForm;
 import jp.co.sample.form.LoginForm;
+import jp.co.sample.service.AdministratorService;
 
 @Controller
 @RequestMapping("/")
 public class AdministratorController {
+	
+	@Autowired
+	private HttpSession session;
 	
 	@ModelAttribute
 	public InsertAdministratorForm setUpInsertAdministratorForm() {
@@ -42,6 +50,20 @@ public class AdministratorController {
 		return "administrator/login";
 	}
 	
+	@RequestMapping("/login")
+	public String login(LoginForm form,Model model) {
+		Administrator administrator=administratorService.login(form.getMailAddress(),form.getPassword());
+		
+		if(administrator ==null ) {
+		
+	
+		}
+	}
+}
+		
+		
+	
+	
 	
 
-}
+
--- src/main/java/jp/co/sample/service/AdministratorService.java
+++ src/main/java/jp/co/sample/service/AdministratorService.java
@@ -21,6 +21,11 @@ public class AdministratorService {
 		administratorRepository.insert(administrator);
 	}
 	
+	public Administrator login(String mailAddress,String password) {
+		return administratorRepository.findByMailAddressAndPassword(mailAddress,password);
+		
+	}
+	
 	
 	
 
