67280cae380cf4678cd0674242ae79da2bca1375
fukumoto <yudai.fukumoto@rakus-partners.co.jp>
2022-05-16 16:56:58 +0900

    logout

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -65,6 +65,16 @@ public class AdministratorController {
 			return "forward:/employee/showList";
 		}
 	}
+	
+	@RequestMapping("/logout")
+	public String logout() {
+		session.invalidate();
+		return "redirect:/";
+				
+		
+		
+		
+	}
 }
 
 
